58c1223ca7ae43187b4b4299b0ad5fd938ddc7f9
Amine <aminetabia98@gmail.com>
2022-05-03 15:28:35 +0000

    Build 1.0
    First Build

---
+++ build/precache-manifest.b8a7a062c7c145262cebb047369668c7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3128d731e58e0537ecfc9e379f9bd0d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d138c26a1c7e0923b2bd",
+    "url": "/static/css/2.905c8d52.chunk.css"
+  },
+  {
+    "revision": "f0c84ea3bdd3f27241ab",
+    "url": "/static/css/main.2c1dee8e.chunk.css"
+  },
+  {
+    "revision": "d138c26a1c7e0923b2bd",
+    "url": "/static/js/2.16521875.chunk.js"
+  },
+  {
+    "revision": "c00e2d29da58185f953e",
+    "url": "/static/js/3.41520e07.chunk.js"
+  },
+  {
+    "revision": "f0c84ea3bdd3f27241ab",
+    "url": "/static/js/main.8a422db1.chunk.js"
+  },
+  {
+    "revision": "f1d77ffd8a7d474750f8",
+    "url": "/static/js/runtime~main.957808d2.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
